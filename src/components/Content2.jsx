73533cf07aac3de4b6920b8515f7052ae8959801
Sailesh <shailesh.mrzn@gmail.com>
2024-03-15 12:28:31 +0545

    animation update v1.1.0

--- src/components/Content2.jsx
+++ src/components/Content2.jsx
@@ -2,21 +2,19 @@ import React, { useRef, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 
 function Content2() {
-  const { ref: refTxt, inView: inViewTxt } = useInView();
-  const { ref: refImg, inView: inViewImg } = useInView();
+  const { ref: refTxt, inView } = useInView();
+  const { ref: refImg } = useInView();
 
   return (
     <>
-      <div
-        className="mt-[8rem] mx-[6rem] font-primary text-gray-500 relative z-20"
-        ref={refTxt}
-      >
+      <div className="mt-[8rem] mx-[6rem] font-primary text-gray-500 relative z-20">
         <p className="text-[2rem]">Welcome to sFitness</p>
         <p
-          className={`font-bold text-[6rem] max-w-[70%] opacity-0 translate-y-40 ${
-            inViewTxt
+          ref={refTxt}
+          className={`font-bold text-[6rem] max-w-[70%] opacity-0 ${
+            inView
               ? "opacity-100 translate-y-0 transition-all ease-in-out duration-500 delay-300"
-              : ""
+              : " translate-y-40 opacity-0"
           }`}
         >
           Fitness is not a destination. It is a way of life
@@ -27,8 +25,8 @@ function Content2() {
           src="https://images.pexels.com/photos/4398382/pexels-photo-4398382.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt=""
           className={`absolute xl:right-[-20%] md:right-[-40%] top-[20%] h-[70%] z-0 opacity-0 ${
-            inViewImg
-              ? "xl:right-[0%] md:right-[-20%] opacity-100 transition-all ease-in-out duration-500 delay-200"
+            inView
+              ? "xl:right-[0%] md:right-[-20%] opacity-100 transition-all ease-in-out duration-500 delay-300"
               : "xl:right-[-20%] md:right-[-40%] opacity-0 transition-all ease-out duration-500 delay-150"
           }`}
         />
